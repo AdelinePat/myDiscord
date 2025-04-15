68f49e241d1ef0c30bf8dd210fb41a528d7ed6cb
AeRiaSaito <florence.navet@laplateforme.io>
2025-04-15 11:47:06 +0200

    feat : gtk fix minimum  size window

--- example.c
+++ example.c
@@ -1,10 +1,22 @@
+// GTK is a multi-platform toolkit for creating graphical user interfaces.
+// This header includes all the necessary GTK+ 3 functions, widgets, and macros
+// needed to build GUI applications in C.
 #include <gtk/gtk.h>
 
 static void activate(GtkApplication* app, gpointer user_data) {
     GtkWidget *window;
     window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "My_discord_blabla");
-    gtk_window_set_default_size(GTK_WINDOW(window), 700, 500);
+    gtk_window_set_title(GTK_WINDOW(window), "My_discord_blabla2");
+
+    // Default window size
+    gtk_window_set_default_size(GTK_WINDOW(window), 1280, 720);
+
+    // Set minimum window size (prevents resizing below this size)
+    GdkGeometry hints;
+    hints.min_width = 500;
+    hints.min_height = 300;
+    gtk_window_set_geometry_hints(GTK_WINDOW(window), NULL, &hints, GDK_HINT_MIN_SIZE);
+
     gtk_widget_show_all(window);
 }
 
