ed356554411c9dc3db5ea9efba968792d66e0424
AeRiaSaito <florence.navet@laplateforme.io>
2025-04-22 07:59:29 +0200

    add TODO for function connection

--- login_window.h
+++ login_window.h
@@ -4,5 +4,7 @@
 #include <gtk/gtk.h>
 
 void show_login_window(GtkApplication *app);
+// TODO: Déclarer ici la fonction validate_credentials une fois créée
+// bool validate_credentials(const gchar *username, const gchar *password);
 
 #endif
