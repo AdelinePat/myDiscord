b7bbcf87608961d0263bd067e8769a015af884c0
Adeline <adeline.patenne@hotmail.fr>
2025-04-24 11:33:42 +0200

    feat: WSAStartup first attempt using socket

--- model/source/connection_sockets_test.c
+++ model/source/connection_sockets_test.c
@@ -1,25 +1,17 @@
+#include "../header/connection_sockets_test.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+int main ()
+{
+    WSADATA wsaData;
+    int iResult;
 
-#include <unistd.h>
-
-#include <sys/socket.h>
-#include <netinet/in.h>
-
-#include <netdb.h>
-#include <netinet/in.h>
-
-// CREATE SOCKET DESCRIPTOR sockfd = Socket File Descriptor : int sockfd = socket(domain, type, protocol);
-int sockfd = socket(AF_LOCAL, SOCK_STREAM, 0);
-
-// CREATE A SOCKET OPTION TO MANIPULATE FILE DESCRIPTOR
-int setsockopt(int sockfd, int level, int optname, const void *optval, socklen_t optlen);
-
-int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
-
-// backlock = The backlog, defines the maximum length to which the queue of pending connections for sockfd may grow. If a connection request arrives when the queue is full, the client may receive an error with an indication of ECONNREFUSED.
-int listen(int sockfd, int backlog);
-
-
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed: %d\n", iResult);
+        return 1;
+    }
+    printf("Yay");
+    WSACleanup();
+    return 0;
+}
