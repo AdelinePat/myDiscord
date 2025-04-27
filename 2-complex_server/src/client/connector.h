f382d9df5f6ab263a1c9c9a4416758c7610c6488
jolyne-mangeot <joseph.mangeot@laplateforme.io>
2025-04-27 23:37:14 +0200

    feat: add communication between server and clients for login and message sending (missing register)

--- 2-complex_server/src/client/connector.h
+++ 2-complex_server/src/client/connector.h
@@ -1,7 +1,11 @@
-#ifndef LISTENER_H
-#define LISTENER_H
+#ifndef CONNECTOR_H
+#define CONNECTOR_H
+#include <winsock2.h>
+#include "utils.h"
 
+void client_start();
+int login_attempts(SOCKET sock);
 void *receive_messages(void *arg);
-void send_message(int sock, int client_id, const char *text);
+void send_message(Client_data *client, char text[1024]);
 
 #endif
