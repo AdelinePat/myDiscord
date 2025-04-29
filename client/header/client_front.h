0ebf471b85877c53532ddb58636904d85d509cc1
Adeline <adeline.patenne@hotmail.fr>
2025-04-29 12:03:58 +0200

    fix: reintegrate gpointer user_data for login_window
    todo : reintegrate gpointer EVERYWHERE ELSE

--- client/header/client_front.h
+++ client/header/client_front.h
@@ -5,7 +5,7 @@
 #include "../../controller/header/utils.h"
 
 // static void on_activate(GtkApplication *app, gpointer user_data, Login_infos *login_info);
-static void on_activate(Login_package_for_front *login_pack);
+static void on_activate(GtkApplication *app, gpointer user_data);
 int launch_front(Login_infos *login_info);
 
 #endif
