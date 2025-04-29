4c7393ae89d3ebd970764b57ab6cf9e713fea4a0
AeRiaSaito <florence.navet@laplateforme.io>
2025-04-29 10:51:12 +0200

    feat : merge change

--- client/header/client_front.h
+++ client/header/client_front.h
@@ -4,7 +4,8 @@
 #include "../header/login_window.h"
 #include "../../controller/header/utils.h"
 
-static void on_activate(GtkApplication *app, gpointer user_data, Login_infos *login_info);
+// static void on_activate(GtkApplication *app, gpointer user_data, Login_infos *login_info);
+static void on_activate(Login_package_for_front *login_pack);
 int launch_front(Login_infos *login_info);
 
 #endif
