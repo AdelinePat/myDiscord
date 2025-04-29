702d052a8b3e4df3809f567b16318ec963a4c4f0
Adeline <adeline.patenne@hotmail.fr>
2025-04-29 10:24:20 +0200

    fix: getting rid of return after check for malloc

--- client/source/front/client_front.c
+++ client/source/front/client_front.c
@@ -7,14 +7,29 @@
 //     show_login_window(app);  // Start with the login screen
 // }
 
-static void on_activate(GtkApplication *app, gpointer user_data, Login_infos *login_info) {
+static void on_activate(Login_package_for_front *login_pack) {
     // (void)user_data;
-    show_login_window(app, user_data, login_info);  // Start with the login screen
+    // GtkApplication *app = login_pack->app;
+    // gpointer user_data = login_pack->data;
+    // Login_infos login_info = login_pack->login_info;
+    show_login_window(login_pack);  // Start with the login screen
 }
 
 int launch_front(Login_infos *login_info) {
     GtkApplication *app = gtk_application_new("org.my.discord", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
+
+    gpointer user_data = NULL;
+    Login_package_for_front *login_pack = malloc(sizeof(Login_package_for_front));
+    if (login_pack == NULL) {
+        g_warning("Failed to allocate memory for login_pack.");
+        printf("Failed to allocate memory for login_pack."); // Or handle the error in an appropriate way
+    }
+    
+    login_pack->login_info = login_info;
+    login_pack->app = app;
+    login_pack->data = user_data;
+
+    g_signal_connect(app, "activate", G_CALLBACK(on_activate), login_pack);
     // int status = g_application_run(G_APPLICATION(app), argc, argv);
     int status = g_application_run(G_APPLICATION(app), 0, NULL);
     g_object_unref(app);
