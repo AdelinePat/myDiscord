da78d246cc733f55e967b450500b75cd474e2016
jolyne-mangeot <jolyne.mangeot@gmail.com>
2025-04-30 16:17:22 +0200

    feat: add communication between server and clients for login and message sending (missing register)

--- client/header/connector.h
+++ client/header/connector.h
@@ -9,6 +9,8 @@ SOCKET client_start();
 // int login_attempts(SOCKET sock);
 int login_attempts(Login_package_for_front *login_pack);
 void *receive_messages(void *arg);
+void receive_client_data(Login_package_for_front *login_pack);
+void broadcast_notifications_receiver_start(Login_package_for_front *login_pack);
 void send_message(Client_data *client, char text[1024]);
 
 #endif
