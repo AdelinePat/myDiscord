c0a84826eb9de915e67510115792537fa7ef20f7
Adeline <adeline.patenne@hotmail.fr>
2025-05-01 13:18:40 +0200

    fix: register_user work but gtk crashes afterward

--- client/header/connector.h
+++ client/header/connector.h
@@ -13,6 +13,6 @@ void *receive_messages(void *arg);
 void receive_client_data(Login_package_for_front *login_pack);
 void broadcast_notifications_receiver_start(Login_package_for_front *login_pack);
 void send_message(Client_data *client, char text[1024]);
-SendType register_attempts(Login_package_for_front *login_pack);
+// SendType register_attempts(Login_package_for_front *login_pack);
 
 #endif
--- server/source/register_user.c
+++ server/source/register_user.c
@@ -16,7 +16,7 @@ int does_user_exists(Login_infos* login_info) {
         exit(1);
     }
 
-    printf("valeur de user_name %s et de email %s avant la requete\n", login_info->username, login_info->email);
+    printf("valeur de user_name %s et de email %s et password : %s avant la requete\n", login_info->username, login_info->email, login_info->password);
     snprintf(query, final_size_query,
         "SELECT COUNT(user_name) FROM users\n\
     WHERE (user_name = '%s' OR email = '%s')", login_info->username, login_info->email);
@@ -56,7 +56,7 @@ PGresult * create_new_user_query(PGconn *conn, Client_package *client_package)
     int nFields;
     int final_size_query = snprintf(NULL, 0,
         "INSERT INTO users (user_name, email, password)\n\
-        VALUES(%s, %s, %s);", client_package->login_info->username, client_package->login_info->email, client_package->login_info->password) +1;
+        VALUES('%s', '%s', '%s');", client_package->login_info->username, client_package->login_info->email, client_package->login_info->password) +1;
 
     char *query = malloc(final_size_query);
     if (query == NULL) {
@@ -66,16 +66,18 @@ PGresult * create_new_user_query(PGconn *conn, Client_package *client_package)
     }
     snprintf(query, final_size_query,
         "INSERT INTO users (user_name, email, password)\n\
-        VALUES(%s, %s, %s);", client_package->login_info->username, client_package->login_info->email, client_package->login_info->password);
+        VALUES('%s', '%s', '%s');", client_package->login_info->username, client_package->login_info->email, client_package->login_info->password);
 
     res = PQexec(conn, query);
     free(query);
     ExecStatusType resStatus = PQresultStatus(res);
 
     if (resStatus != PGRES_COMMAND_OK)
-    {
-        printf("valeur de resStatus : %d", resStatus);
-        printf("il y a eu une erreur dans la requête de création de user");
+    {   
+        // char *PQresStatus(ExecStatusType status)
+        // printf("valeur de resStatus : %s", resStatus);
+        fprintf(stderr, "ERROR: %s\n", PQerrorMessage(conn));
+        printf("il y a eu une erreur dans la requête de création de user\n");
         PQclear(res);
         PQfinish(conn);
         exit(1);
@@ -90,10 +92,10 @@ void create_new_user_db(Client_package* client_package) {
 
     if(does_user_exists(client_package->login_info) != 0)
     {
-        printf("Impossible de créer un utilisateur qui existe déjà");
+        printf("Impossible de créer un utilisateur qui existe déjà\n");
     } else {
         res = create_new_user_query(conn, client_package);
-        printf("creation du nouveal utilisateur = SUCCES");       
+        printf("creation du nouveal utilisateur = SUCCES\n");       
         PQclear(res);
         PQfinish(conn);
     }
