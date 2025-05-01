41b56b1171c73f4f658c2faca186f4dfab62ec49
enolaworld <enolaworld@gmail.com>
2025-05-01 18:59:29 +0200

    feat: modified hash_login.h

--- client/header/hash_login.h
+++ client/header/hash_login.h
@@ -3,6 +3,4 @@
 
 void hash_password_sha256(const char *password, char *output_buffer);
 
-int unhex_sha256_hash(const char *hex_string, unsigned char *output_hash);
-
 #endif
--- database_files/test_db.sql
+++ database_files/test_db.sql
@@ -1,2 +1,6 @@
 -- Active: 1746033929458@@127.0.0.1@5432@whispr@public
 select * FROM users;
+
+SELECT * FROM channels;
+
+SELECT * fROM channels_access
